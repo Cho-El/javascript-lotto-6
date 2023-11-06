530fa057d5f5aeea8ce449c52224a60ac9147a03
Cho-El <syzzang21c@gmail.com>
2023-11-07 07:06:00 +0900

    feat: promise 연습 객체

---
+++ src/promisePractice.js
@@ -0,0 +1,63 @@
+const asyncFn = async () => {
+    console.log('start');
+    const rj1 = await rejectFn1()
+    .then()
+    .catch(err => { return `catch in rejectFn1 - ${err}`; });
+    console.log(rj1);
+    const rj2 = await rejectFn2().catch(err => { return `catch in rejectFn2 - ${err}`;  });
+    console.log(rj2);
+  }
+  
+  const rejectFn1 = () => Promise.resolve('rejectFn1 resolved');
+  const rejectFn2 = () => Promise.reject('rejectFn2 rejected');
+  
+  asyncFn()
+    .then(() => {
+      console.log('next asynFn()');
+    })
+    .catch((e) => {
+      console.log('asyncFn catch', e);
+    })
+
+async function a() {
+    try {
+        // 비동기 작업을 수행하는 a 메소드 로직
+        await someAsyncOperation();
+        console.log("a 메소드 정상 실행");
+        await b();
+    } catch (error) {
+        console.error("a 메소드 오류:", error.message);
+        await a();
+    }
+    }
+    
+async function b() {
+try {
+    // 비동기 작업을 수행하는 b 메소드 로직
+    await someAsyncOperation();
+    console.log("b 메소드 정상 실행");
+    await c();
+} catch (error) {
+    console.error("b 메소드 오류:", error.message);
+    await b();
+}
+}
+
+async function c() {
+// 비동기 작업을 수행하는 c 메소드 로직
+console.log("c 메소드 정상 실행");
+}
+
+async function someAsyncOperation() {
+// 비동기 작업을 수행하는 함수 (예: Promise 반환)
+return new Promise((resolve, reject) => {
+    if (Math.random() < 0.5) {
+    reject(new Error("비동기 작업 오류"));
+    } else {
+    resolve();
+    }
+});
+}
+
+// a 메소드를 호출하여 시작
+a();
